f80ca89c9593c66936ecbf30236a9f3603705db8
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2020-10-26 12:51:19 +0000

    feat: add all psuedo params

--- packages/core/src/util/AWS.ts
+++ packages/core/src/util/AWS.ts
@@ -5,11 +5,31 @@ export const AWS = {
     return Intrinsics.ref('AWS::AccountId');
   },
 
+  get NotificationARNs(): IntrinsicValue {
+    return Intrinsics.ref('AWS::NotificationARNs');
+  },
+
+  get NoValue(): IntrinsicValue {
+    return Intrinsics.ref('AWS::NoValue');
+  },
+
   get Partition(): IntrinsicValue {
     return Intrinsics.ref('AWS::Partition');
   },
 
   get Region(): IntrinsicValue {
     return Intrinsics.ref('AWS::Region');
+  },
+
+  get StackId(): IntrinsicValue {
+    return Intrinsics.ref('AWS::StackId');
+  },
+
+  get StackName(): IntrinsicValue {
+    return Intrinsics.ref('AWS::StackName');
+  },
+
+  get URLSuffix(): IntrinsicValue {
+    return Intrinsics.ref('AWS::URLSuffix');
   },
 };
